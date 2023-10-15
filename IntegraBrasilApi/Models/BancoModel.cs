26dfe05bfc7d391115e83cff663263124fbc8870
Mac7ota <mauro.alves.aguiar@outlook.com>
2023-10-15 17:33:22 -0300

    Primeira versão v1.0

---
+++ IntegraBrasilApi/Models/BancoModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntegraBrasilApi.Models
+{
+    public class BancoModel
+    {
+        /// <summary>
+        /// O uso do JsonProperty especificando Name nele e quando a propriedade nao vai ter o mesmo nome que o json exemplo     City e  Cidade não faz diferença caso o nome seja o mesmo
+        /// </summary>
+        
+        [JsonPropertyName("ispb")]
+        public string? Ispb { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? NomeAbreviado { get; set; }
+
+        [JsonPropertyName("code")]
+        public int? Codigo { get; set; }
+
+        [JsonPropertyName("fullName")]
+        public string? NomeCompleto { get; set; }
+
+
+    }
+}
---
+++ IntegraBrasilApi/Models/EnderecoModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntegraBrasilApi.Models
+{
+    public class EnderecoModel
+    {
+        /// <summary>
+        /// O uso do JsonProperty especificando Name nele e quando a propriedade nao vai ter o mesmo nome que o json exemplo     City e  Cidade não faz diferença caso o nome seja o mesmo
+        /// </summary>
+       
+        [JsonPropertyName("cep")]
+        public string? Cep { get; set; }
+
+        [JsonPropertyName("state")]
+        public string? Estado { get; set; }
+
+        [JsonPropertyName("city")]
+        public string? Cidade { get; set; }
+
+        [JsonPropertyName("neighborhood")]
+        public string? Regiao { get; set; }
+
+        [JsonPropertyName("street")]
+        public string? Rua { get; set; }
+
+        [JsonPropertyName("service")]
+        public string? Servico { get; set; }
+    }
+}
